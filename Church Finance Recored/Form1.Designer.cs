b6e06b551237a7a1b333c36e356b52240bf2e6d3
princeso <donprince4life44@gmail.com>
2018-04-23 15:00:56 +0100

    record

--- Church Finance Recored/Form1.Designer.cs
+++ Church Finance Recored/Form1.Designer.cs
@@ -28,20 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.header_panel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // header_panel
+            // 
+            this.header_panel.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.header_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.header_panel.Location = new System.Drawing.Point(0, 0);
+            this.header_panel.Name = "header_panel";
+            this.header_panel.Size = new System.Drawing.Size(1010, 115);
+            this.header_panel.TabIndex = 0;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1010, 567);
+            this.ClientSize = new System.Drawing.Size(1010, 698);
+            this.Controls.Add(this.header_panel);
+            this.Font = new System.Drawing.Font("Sitka Display", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Church Finance Record";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel header_panel;
     }
 }
 
